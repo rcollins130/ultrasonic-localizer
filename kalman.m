1ddb7ac5da079fc91e13674ea5bcbfc649bc48b1
Ben <webman128@gmail.com>
2023-06-04 21:05:28 -0700

    Added Example Code
    Added Example code

--- kalman.m
+++ kalman.m
@@ -18,3 +18,13 @@ function [x,P] = kalman(x_old,P_old,y)
         P = P_new;
     end
 end
+
+% Add this to code to use
+% x = zeros(4,num_obs);
+% P = zeros(4,4,num_obs);
+% x(:,1) = [y(1,1);y(2,1);0;0];
+% P(:,:,1) = [1000,0,0,0;0,1000,0,0;0,0,1000,0;0,0,0,1000];
+% 
+% for i = 2:num_obs
+%     [x(:,i),P(:,:,i)] = kalman(x(:,i-1),P(:,:,i-1),y(:,i));
+% end
