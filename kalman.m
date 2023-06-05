0a1af8bae5f1230231904e801d22832e30b94115
Ben <webman128@gmail.com>
2023-06-04 21:04:21 -0700

    Create kalman.m
    Created kalman filter for linear motion with x,y movement uncorrelated

---
+++ kalman.m
@@ -0,0 +1,20 @@
+function [x,P] = kalman(x_old,P_old,y)
+
+    R = [0.1,0;0,0.1];
+    A = [1,0,1,0;0,1,0,1;0,0,1,0;0,0,0,1];
+    H = [1,0,0,0;0,1,0,0];
+
+    xp = A*x_old;
+    Pp = A*P_old*A';
+    K = (Pp*H')/(H*Pp*H'+R);
+    x_new = xp + K * (y-H*xp);
+    P_new = Pp - K*H*Pp;
+
+    if mvncdf(x_new,xp,Pp) < 0.05
+        x = x_old;
+        P = P_old;
+    else
+        x = x_new;
+        P = P_new;
+    end
+end
